b9cf2ed3e522ea5f3af4550ba2f80879b082b5b4
MemelyPepeartly <memelypepeartly@gmail.com>
2025-02-16 01:06:26 -0500

    UI leaf parser

--- TACC/Scripts/Stem.cs
+++ TACC/Scripts/Stem.cs
@@ -9,18 +9,43 @@ public partial class Stem : Node
 
 		AddLeaf("res://TACC/Leaves/CutsceneLeaf.tscn");
 		AddLeaf("res://TACC/Leaves/MapLeaf.tscn");
-		AddLeaf("res://TACC/Leaves/MenuFactoryLeaf.tscn");
+		AddLeafAsUI("res://TACC/Leaves/MenuFactoryLeaf.tscn");
 	}
 
 	public void AddLeaf(string leafPath)
 	{
-		// Load a leaf (modular component)
 		PackedScene leafScene = GD.Load<PackedScene>(leafPath);
 		if (leafScene != null)
 		{
 			Node leafInstance = leafScene.Instantiate();
 			AddChild(leafInstance);
 			GD.Print($"Leaf {leafInstance.Name} added to Stem.");
+		}
+		else
+		{
+			GD.PrintErr($"Failed to load leaf: {leafPath}");
+		}
+	}
+
+	// Method to add UI leaves inside a CanvasLayer
+	  public void AddLeafAsUI(string leafPath)
+	{
+		PackedScene leafScene = GD.Load<PackedScene>(leafPath);
+		if (leafScene != null)
+		{
+			Node leafInstance = leafScene.Instantiate();
+			leafInstance.Name = "MenuFactoryLeaf";
+
+			var canvasLayer = new CanvasLayer
+			{
+				Name = "CanvasLayer",
+				Layer = 1
+			};
+
+			canvasLayer.AddChild(leafInstance);
+			AddChild(canvasLayer);
+
+			GD.Print($"UI Leaf {leafInstance.Name} added to Stem inside CanvasLayer.");
 		}
 		else
 		{
