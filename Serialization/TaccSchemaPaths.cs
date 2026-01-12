ecae9ebb24ce3d1777182f03f17fa25fe50e6afd
MemelyPepeartly <memelypepeartly@gmail.com>
2026-01-11 23:36:51 -0500

    flattened directory

---
+++ Serialization/TaccSchemaPaths.cs
@@ -0,0 +1,13 @@
+namespace TACCsharp.TACC.Serialization
+{
+	public static class TaccSchemaPaths
+	{
+		public const string Background = "res://addons/tacc/Docs/schemas/background.schema.json";
+		public const string Cutscene = "res://addons/tacc/Docs/schemas/cutscene.schema.json";
+		public const string Hud = "res://addons/tacc/Docs/schemas/hud.schema.json";
+		public const string Map = "res://addons/tacc/Docs/schemas/map.schema.json";
+		public const string Menu = "res://addons/tacc/Docs/schemas/menu.schema.json";
+		public const string Music = "res://addons/tacc/Docs/schemas/music.schema.json";
+		public const string Sprite = "res://addons/tacc/Docs/schemas/sprite.schema.json";
+	}
+}
